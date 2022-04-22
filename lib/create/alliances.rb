a6defd66e07561d9bb6c304fa566d74eb46f81d8
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-04-22 13:23:22 -0400

    Update alliances.rb

--- lib/create/alliances.rb
+++ lib/create/alliances.rb
@@ -50,8 +50,8 @@ class Sunny
             event.respond("There were no matches.") if choices.size == 0
             break if choices.size == 0
 
-            event.respond("Not enough members for an alliance!") if choices.size < 3
-            break if choices.size < 3
+            event.respond("Not enough members for an alliance!") if choices.size < 2
+            break if choices.size < 2
 
             choices.map! do |choice|
                 Player.find_by(id: choice)
