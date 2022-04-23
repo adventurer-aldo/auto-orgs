28e2e033a17b60638f832070189369a0e585f932
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-04-22 22:35:03 -0400

    Update run.rb

--- run.rb
+++ run.rb
@@ -18,10 +18,6 @@ end
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 require_relative 'settings'
-require_relative 'petra'
-require_relative 'donovan'
-require_relative 'sailor'
-require_relative 'augur'
 require_all 'lib'
 
 Augur.run
