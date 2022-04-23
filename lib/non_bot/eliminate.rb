16e1fd1f7f516e96731f8c78867577b28f80e9e8
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-04-23 15:11:11 -0400

    Making life easier
    In favor of not having different codes do the same thing and having to keep up with modifying every single one of them, I''m making the elimination be a function that is assessed by the different methods of doing so. Might do the same for rocks.

---
+++ lib/non_bot/eliminate.rb
@@ -0,0 +1,51 @@
+class Sunny
+
+    def eliminate(loser,event)
+        tribe = Tribe.find_by(id: loser.tribe)
+        if Setting.last.game_stage == 1
+            loser.update(status: 'Jury', inventory: [])
+            user = BOT.user(loser.user_id).on(event.server)
+            
+            user.remove_role(tribe.role_id)
+            user.remove_role(964564440685101076)
+            user.add_role(965717073454043268)
+        else
+            loser.update(status: 'Out', inventory: [])
+            user = BOT.user(loser.user_id).on(event.server)
+            
+            user.remove_role(tribe.role_id)
+            user.remove_role(964564440685101076)
+            user.add_role(965717099202904064)
+        end
+
+        alliances = Alliance.where("#{loser.id} = ANY (players)")
+        alliances.each do |alliance|
+            alliance.update(players: alliance.players - [loser.id])
+            channel = BOT.channel(alliance.channel_id)
+            if alliance.players.size < 4 || alliance.players.size == event.server.role(Tribe.find_by(id: loser.tribe).role_id).members.size
+                channel.parent = ARCHIVE
+                BOT.send_message(channel.id, ":ballot_box_with_check: **This channel has been archived!**")
+                channel.permission_overwrites.each do |role, perms|
+                    unless role.id == loser.user_id
+                        channel.define_overwrite(event.server.member(role), 0, 3072)
+                    else
+                        channel.define_overwrite(event.server.member(loser.user_id), 0, 3072)
+                    end
+                end
+            else
+                BOT.send_message(channel.id, ":broken_heart: **#{loser.name} removed...**")
+                channel.permission_overwrites.each do |role, perms|
+                    unless role.id == loser.user_id
+                        channel.define_overwrite(event.server.member(role), 3072, 0)
+                    else
+                        channel.define_overwrite(event.server.member(loser.user_id), 0, 3072)
+                    end
+                end
+            end
+        end
+        BOT.channel(loser.confessional).name = "#{rank}th-" + BOT.channel(loser.confessional).name
+        BOT.channel(loser.submissions).name = "#{rank}th-" + BOT.channel(loser.submissions).name
+        Player.where(status: ALIVE).update(status: 'In')
+    end
+
+end
