4f56241f2b66185c7920b20976d45b370dfb889f
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-04-28 18:10:08 -0400

    Update item_effects.rb

--- lib/non_command/item_effects.rb
+++ lib/non_command/item_effects.rb
@@ -36,6 +36,7 @@ ah yes, the find command. although, a find command kinda...doesn't jive right no
         when 'Now'
             item.functions.each do |function|
                 council = Council.find_by(stage: [0,1], season: Setting.last.season)
+                player = Player.find_by(id: item.owner, season: Setting.last.season)
                 case function
                 when 'extra_vote'
                     event.respond("Are you sure?")
@@ -47,7 +48,6 @@ ah yes, the find command. although, a find command kinda...doesn't jive right no
                     event.respond("Okay!") unless CONFIRMATIONS.include? confirmation.message.content.downcase
                     break unless CONFIRMATIONS.include? confirmation.message.content.downcase
 
-                    player = Player.find_by(id: item.owner, season: Setting.last.season)
                     vote = Vote.find_by(council: council.id, player: player.id)
 
                     BOT.channel(council.channel_id).send_embed do |embed|
@@ -59,8 +59,116 @@ ah yes, the find command. although, a find command kinda...doesn't jive right no
                     item.update(targets: [player.id], owner: 0)
 
                 when 'steal_vote'
+                    targets = []
+                    enemies = Vote.where(council: council.id, allowed: Array(1..10)).excluding(Vote.where(player: player.id)).map(&:player).map { |n| Player.find_by(id: n) }
+                    enemies.delete(nil)
+        
+                    text = enemies.map do |en|
+                        "**#{en.id}** — #{en.name}"
+                    end
+
+                    event.channel.send_embed do |embed|
+                        embed.title = "Who would you like to play it on?"
+                        embed.description = text.join("\n")
+                        embed.color = event.server.role(Tribe.find_by(id: player.tribe).role_id).color
+                    end
+    
+                    await = event.user.await!(timeout: 80)
+    
+                    event.respond("You didn't pick a target...") if await == nil
+                    break if await == nil
+    
+                    text_attempt = enemies.map(&:name).filter { |nome| nome.downcase.include? content }
+                    id_attempt = enemies.map(&:id).filter { |id| id == content.to_i }
+                    if text_attempt.size == 1
+                        targets << Player.find_by(name: text_attempt[0], season: Setting.last.season, status: ALIVE)
+                    elsif id_attempt.size == 1
+                        targets << Player.find_by(id: id_attempt[0])
+                    else
+                        event.respond("There's no single seedling that matches that.") unless content == ''
+                    end
+                    
+                    unless targets == []
+                        event.respond("You're about to use **#{item.name}** on **#{targets.map(&:name).join(', ')}**. Are you sure?")
+                        confirmation = event.user.await!(timeout: 50)
+    
+                        event.respond("You didn't confirm. Try again if you want to play it.") if confirmation.nil?
+                        break if confirmation.nil?
+    
+                        event.respond("Okay!") unless CONFIRMATIONS.include? confirmation.message.content.downcase
+                        break unless CONFIRMATIONS.include? confirmation.message.content.downcase
+
+                        event.respond("You used **#{item.name}** on **#{targets.map(&:name).join('**, **').gsub(player.name,'yourself')}**")
+                        Vote.where(council: council, player: targets.map(&:id)).each do |vote|
+                            vote.update(allowed: vote.allowed - 1, votes: vote.votes.delete_at(vote.votes.size - 1))
+                        end
+
+                        BOT.channel(council.channel_id).send_embed do |embed|
+                            embed.title = "**#{player.name} used #{item.name} on #{targets.map(&:name).join(', ')}!**"
+                            embed.description = "This advantage steals one of the target's votes and allows the owner of this advantage to cast an extra vote with the stolen parchment.."
+                            embed.color = event.server.role(TRIBAL_PING).color
+                        end
+
+                        item.update(owner: 0, targets: targets.map(&:id))
+                    else
+                        event.respond("Playing this item failed!")
+                    end
 
                 when 'block_vote'
+                    targets = []
+                    enemies = Vote.where(council: council.id, allowed: Array(1..10)).excluding(Vote.where(player: player.id)).map(&:player).map { |n| Player.find_by(id: n) }
+                    enemies.delete(nil)
+        
+                    text = enemies.map do |en|
+                        "**#{en.id}** — #{en.name}"
+                    end
+
+                    event.channel.send_embed do |embed|
+                        embed.title = "Who would you like to play it on?"
+                        embed.description = text.join("\n")
+                        embed.color = event.server.role(Tribe.find_by(id: player.tribe).role_id).color
+                    end
+    
+                    await = event.user.await!(timeout: 80)
+    
+                    event.respond("You didn't pick a target...") if await == nil
+                    break if await == nil
+    
+                    text_attempt = enemies.map(&:name).filter { |nome| nome.downcase.include? content }
+                    id_attempt = enemies.map(&:id).filter { |id| id == content.to_i }
+                    if text_attempt.size == 1
+                        targets << Player.find_by(name: text_attempt[0], season: Setting.last.season, status: ALIVE)
+                    elsif id_attempt.size == 1
+                        targets << Player.find_by(id: id_attempt[0])
+                    else
+                        event.respond("There's no single seedling that matches that.") unless content == ''
+                    end
+                    
+                    unless targets == []
+                        event.respond("You're about to use **#{item.name}** on **#{targets.map(&:name).join(', ')}**. Are you sure?")
+                        confirmation = event.user.await!(timeout: 50)
+    
+                        event.respond("You didn't confirm. Try again if you want to play it.") if confirmation.nil?
+                        break if confirmation.nil?
+    
+                        event.respond("Okay!") unless CONFIRMATIONS.include? confirmation.message.content.downcase
+                        break unless CONFIRMATIONS.include? confirmation.message.content.downcase
+
+                        event.respond("You used **#{item.name}** on **#{targets.map(&:name).join('**, **').gsub(player.name,'yourself')}**")
+                        Vote.where(council: council, player: targets.map(&:id)).each do |vote|
+                            vote.update(allowed: vote.allowed - 1, votes: vote.votes.delete_at(vote.votes.size - 1))
+                        end
+
+                        BOT.channel(council.channel_id).send_embed do |embed|
+                            embed.title = "**#{player.name} used #{item.name} on #{targets.map(&:name).join(', ')}!**"
+                            embed.description = "This advantage blocks one of the target's votes."
+                            embed.color = event.server.role(TRIBAL_PING).color
+                        end
+
+                        item.update(owner: 0, targets: targets.map(&:id))
+                    else
+                        event.respond("Playing this item failed!")
+                    end
 
                 end
             end
@@ -110,7 +218,7 @@ ah yes, the find command. although, a find command kinda...doesn't jive right no
                         event.respond("You're now using **#{item.name}** on **#{targets.map(&:name).join('**, **').gsub(player.name,'yourself')}**\nPlay it again if you want to cancel it.")
                         item.update(targets: targets.map(&:id))
                     else
-        
+                        event.respond("Playing this item failed!")
                     end
                     
                 when 'idol_nullifier'
