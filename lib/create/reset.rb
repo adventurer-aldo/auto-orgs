8a3489da315f07ca94560835ce4b77e4e3f3eb51
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2023-06-06 23:35:26 +0200

    clue

--- lib/create/reset.rb
+++ lib/create/reset.rb
@@ -1,21 +1,21 @@
 class Sunny
 
-  BOT.command :clean, description: "Delete all Data except Settings." do |event|
+  BOT.command :clean, description: 'Delete all Data except Settings.' do |event|
     break unless HOSTS.include? event.user.id
+
     Vote.destroy_all
     Council.destroy_all
-    Clue.destroy_all
     Item.destroy_all
     Alliance.destroy_all
     Player.destroy_all
     Tribe.destroy_all
-    Score.destroy_all
-    Challenge.destroy_all
-    return "All Data has been destroyed successfuly!"
+    return 'All Data has been destroyed successfuly!'
   end
 
-  BOT.command :prune, description: "Cleans up a channel." do |event|
+  BOT.command :prune, description: 'Cleans up a channel.' do |event|
     break unless HOSTS.include? event.user.id
+
+
     event.channel.prune(100)
     return
 
