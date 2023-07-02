d2f1779a15c82aee79ba436c3f37ca27aa4a0e6f
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2023-07-02 22:05:12 +0200

    am

--- models/tribe.rb
+++ models/tribe.rb
@@ -2,4 +2,5 @@ class Tribe < ActiveRecord::Base
   has_many :players, foreign_key: 'tribe_id'
   has_many :councils, foreign_key: 'tribe_id'
   has_many :votes, through: :councils
+  belongs_to :season, foreign_key: 'season_id'
 end
--- models/vote.rb
+++ models/vote.rb
@@ -1,3 +1,4 @@
 class Vote < ActiveRecord::Base
   belongs_to :council, foreign_key: 'council_id'
+  belongs_to :player, foreign_key: 'player_id'
 end
