4932dad59f84f16bcf07a3a29b195f43c687cbe1
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2024-12-29 17:00:30 +0200

    Create idol_clues.rb

---
+++ lib/misc/idol_clues.rb
@@ -0,0 +1,63 @@
+class Sunny
+  # Tomatoes
+  BOT.command(:casteways, description: 'Clue.') do |event|
+    event.respond("They're unorthodox. Why is there a debate over whether they're fruits or vegetables? This is `strange`. Very `strange`. Really `strange`!")
+  end
+  
+  BOT.command(:strange_strange_strange, description: 'Clue.') do |event|
+    event.respond("The pa?h of the t?mat?es wil? require you ?? exercise some of your head. Fruit? Vegetable? You'll need t? unscramble the mysteries surr?unding this edib?e!")
+  end
+  
+  BOT.command(:tooltool, description: 'Clue.') do |event|
+    event.respond("Yrenal Rehte")
+  end
+  
+  BOT.command(:nearly_there, description: 'Clue.') do |event|
+    event.respond("What's the name of the decision that holds that Tomatoes are classified as **Vegetables**?")
+  
+  BOT.command(:nix_v_hedden, description: 'Clue.') do |event|
+    event.respond("https://www.jigsawplanet.com/?rc=play&pid=15eac5aa9715")
+  end
+
+  # Lettuces
+  BOT.command(:seedlings, description: 'Clue.') do |event|
+    event.respond("Lettuces, between all the others, are clearly the ones most worthy of the title of `vegetables`. I mean, look at all that green? The other `vegetables` don't have that! They're all colorful and rainbow-like. Not obviously `vegetables` at all!")
+  end
+
+  BOT.command(:vegetables_vegetables_vegetables, description: 'Clue.') do |event|
+    event.respond("The pa?h of the l?ttuces will ?xp?ore your origins. The idol was always ?h?re within you, lettuc?s. Seek aid about who you are, and you'?l have it.")
+  end
+
+  BOT.command(:teelteel, description: 'Clue.') do |event|
+    event.respond("Het Ryve Geinignbn")
+  end
+
+  BOT.command(:the_very_beginning, description: 'Clue.') do |event|
+    event.respond("https://www.jigsawplanet.com/?rc=play&pid=2fb4e8f8985f")
+  end
+
+  BOT.command(:i_am_a_vegetable, description: 'Clue.') do |event|
+    event.respond("The answer was always within you. But you must seek it yourself. What is your real name? Do not be afraid to ask Science...")
+  end
+
+  # Carrots
+  BOT.command(:disboard_org, description: 'Clue.') do |event|
+    event.respond("It was within your `sight`. The odd one out. Carrots are said to be helpful for your `sight`. Never lose `sight` of your goals.")
+  end
+  
+  BOT.command(:disboard, description: 'Clue.') do |event|
+    event.respond("It was within your `sight`. The odd one out. Carrots are said to be helpful for your `sight`. Never lose `sight` of your goals.")
+  end
+  
+  BOT.command(:sight_sight_sight, description: 'Clue.') do |event|
+    event.respond("Test the mettle of your sight as a carrot and type out the beginning of every 𝐕𝐞𝐠𝐞𝐭𝐚𝐛𝐥𝐞𝐬.")
+  end
+
+  BOT.command(:tcmec, description: 'Clue.') do |event|
+    event.respond("https://www.jigsawplanet.com/?rc=play&pid=04a304e65f9c")
+  end
+  
+  BOT.command(:judgement, description: 'Clue.') do |event|
+    event.respond("Singularly add your name before this clue, before passing it on...")
+  end
+end
