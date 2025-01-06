7d788c6e88eb68b2caf729bb9aaeba92813b1e75
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-01-06 17:03:08 +0200

    Update idol_clues.rb

--- lib/misc/idol_clues.rb
+++ lib/misc/idol_clues.rb
@@ -1,10 +1,9 @@
 class Sunny
-  # Amaranths
+  # Extra Vote
   BOT.command(:earth_apples, description: 'Clue.') do |event|
     event.respond("The Sunchokes are not the only vegetables referred to as earth apples, and not in english... Who else is there?")
   end
 
-  # Amaranths
   BOT.command(:earth_apple, description: 'Clue.') do |event|
     event.respond("The Sunchokes are not the only vegetables referred to as earth apples, and not in english... Who else is there?")
   end
@@ -24,75 +23,8 @@ class Sunny
   BOT.command(:answer, description: 'Clue.') do |event|
     event.respond("My opposite is...")
   end
-  # Sunchokes
-
-
-
-  # Tomatoes
-  BOT.command(:casteways, description: 'Clue.') do |event|
-    event.respond("They're unorthodox. Why is there a debate over whether they're fruits or vegetables? This is `strange`. Very `strange`. Really `strange`!")
-  end
-  
-  BOT.command(:strange_strange_strange, description: 'Clue.') do |event|
-    event.respond("The pa?h of the t?mat?es wil? require you ?? exercise some of your head. Fruit? Vegetable? You'll need t? unscramble the mysteries surr?unding this edib?e!")
-  end
-  
-  BOT.command(:tooltool, description: 'Clue.') do |event|
-    event.respond("Yrenal Rehte")
-  end
-  
-  BOT.command(:nearly_there, description: 'Clue.') do |event|
-    event.respond("What's the name of the decision that holds that Tomatoes are classified as **Vegetables**?")
-  end
-  
-  BOT.command(:nix_v_hedden, description: 'Clue.') do |event|
-    event.respond("https://www.jigsawplanet.com/?rc=play&pid=15eac5aa9715")
-  end
-
-  # Lettuces
-  BOT.command(:seedlings, description: 'Clue.') do |event|
-    event.respond("Lettuces, between all the others, are clearly the ones most worthy of the title of `vegetables`. I mean, look at all that green? The other `vegetables` don't have that! They're all colorful and rainbow-like. Not obviously `vegetables` at all!")
-  end
-
-  BOT.command(:vegetables_vegetables_vegetables, description: 'Clue.') do |event|
-    event.respond("The pa?h of the l?ttuces will ?xp?ore your origins. The idol was always ?h?re within you, lettuc?s. Seek aid about who you are, and you'?l have it.")
-  end
-
-  BOT.command(:teelteel, description: 'Clue.') do |event|
-    event.respond("Het Ryve Geinignbn")
-  end
-
-  BOT.command(:the_very_beginning, description: 'Clue.') do |event|
-    event.respond("https://www.jigsawplanet.com/?rc=play&pid=2fb4e8f8985f")
-  end
-
-  BOT.command(:i_am_a_vegetable, description: 'Clue.') do |event|
-    event.respond("The answer was always within you. But you must seek it yourself. What is your real name? Do not be afraid to ask Science...")
-  end
-
-  # Carrots
-  BOT.command(:disboard_org, description: 'Clue.') do |event|
-    event.respond("It was within your `sight`. The odd one out. Carrots are said to be helpful for your `sight`. Never lose `sight` of your goals.")
-  end
-  
-  BOT.command(:disboard, description: 'Clue.') do |event|
-    event.respond("It was within your `sight`. The odd one out. Carrots are said to be helpful for your `sight`. Never lose `sight` of your goals.")
-  end
-  
-  BOT.command(:sight_sight_sight, description: 'Clue.') do |event|
-    event.respond("Test the mettle of your sight as a carrot and type out the beginning of every 𝐕𝐞𝐠𝐞𝐭𝐚𝐛𝐥𝐞.")
-  end
-
-  BOT.command(:tcmec, description: 'Clue.') do |event|
-    event.respond("https://www.jigsawplanet.com/?rc=play&pid=04a304e65f9c")
-  end
-  
-  BOT.command(:judgement, description: 'Clue.') do |event|
-    event.respond("Singularly add your name before this clue, before passing it on...")
-  end
 
   # Extra Vote
-  
   BOT.command(:carl_bot, description: 'Clue.') do |event|
     event.respond("An `extra` is the first word that comes to mind when seeing this one. Mee6 is a welcoming one. In the garden of Alvivor, those who can't survive are weeded out.")
   end
@@ -107,27 +39,5 @@ class Sunny
 
   BOT.command(:boost, description: 'Clue.') do |event|
     event.respond("To find that which you seek, do not take your first step. Take all the second, third and fourth steps together, in order.")
-  end
-
-  # Onions
-
-  BOT.command(:trusted_spectator, description: 'Clue.') do |event|
-    event.respond("A similar color, yet different in `essence`. Onions can be yellow, red or white besides purple... but they still have the same `essence`. That is the same for you. No matter what you look like, deep down... you have your own `essence`.")
-  end
-  
-  BOT.command(:essence_essence_essence, description: 'Clue.') do |event|
-    event.respond("Onions have a distant rela?ion w?th carrots. The e?es. Carrots will improve your sigh?, whil? onions may make you cry. Is that a myth? Or t?uth?")
-  end
-
-  BOT.command(:tiyter, description: 'Clue.') do |event|
-    event.respond("Onions in Alvivor are all about the colors. The Hidden Immunity Idol lies in your tribe's own color, but first... look at the whole garden, and tell me the sequence of its rainbow.")
-  end
-
-  BOT.command(:yellow_purple_green_red, description: 'Clue.') do |event|
-    event.respond("https://www.jigsawplanet.com/?rc=play&pid=1b6eb481d5ec")
-  end
-
-  BOT.command(:yellow_purple_green_red, description: 'Clue.') do |event|
-    event.respond("A hexcode is a color's digital code. Find your tribe's digital code among the list of seedlings and the Hidden Immunity Idol shall be yours.")
   end
 end
