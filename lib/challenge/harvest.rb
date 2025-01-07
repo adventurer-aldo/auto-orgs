98dcfe8c6fc41e8b6c91c23a5bff1c4ef0dd8479
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-01-07 20:22:09 +0200

    Update harvest.rb

--- lib/challenge/harvest.rb
+++ lib/challenge/harvest.rb
@@ -26,7 +26,35 @@ class Sunny
     event.respond("Which vegetable is said to be related to enhancing sight/vision?")
   end
 
+  BOT.command :tomatoes do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 0
+    challenge.update(stage: 1)
+
+    event.respond("Which vegetable is said to be related to enhancing sight/vision?")
+  end
+
   BOT.command :carrot do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 1
+    challenge.update(stage: 2)
+    
+    event.respond("Which vegetable is said to make you cry?")
+  end
+
+  BOT.command :carrots do |event|
     break unless event.user.id.player?
 
     player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
@@ -54,6 +82,20 @@ class Sunny
     event.respond("Lactuca sativa is the species name for...")
   end
 
+  BOT.command :onions do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 2
+    challenge.update(stage: 3)
+    
+    event.respond("Lactuca sativa is the species name for...")
+  end
+
   BOT.command :lettuce do |event|
     break unless event.user.id.player?
 
@@ -68,7 +110,35 @@ class Sunny
     event.respond("Also called sunroots, wild sunflowers, topinambur or earth apples...what are those?")
   end
 
+  BOT.command :lettuces do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 3
+    challenge.update(stage: 4)
+    
+    event.respond("Also called sunroots, wild sunflowers, topinambur or earth apples...what are those?")
+  end
+
   BOT.command :sunchokes do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 4
+    challenge.update(stage: 5)
+    
+    event.respond("Cosmopolitan group of more than 50 species which make up the genus of annual or short-lived perennial plants collectively known as?")
+  end
+
+  BOT.command :sunchoke do |event|
     break unless event.user.id.player?
 
     player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
@@ -96,7 +166,35 @@ class Sunny
     event.respond("Said to be significantly superior in Vitamin A than their counterpart. Those are the ...!")
   end
   
+  BOT.command :amaranthus do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 5
+    challenge.update(stage: 6)
+    
+    event.respond("Said to be significantly superior in Vitamin A than their counterpart. Those are the ...!")
+  end
+  
   BOT.command :sweet_potatoes do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 6
+    challenge.update(stage: 7)
+    
+    event.respond("They have some sisters that are said to be spicy. The chilli... In this game, those are the:")
+  end
+  
+  BOT.command :sweet_potato do |event|
     break unless event.user.id.player?
 
     player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
@@ -124,6 +222,48 @@ class Sunny
     event.respond("Seeds that can be eaten, and are contained within a pod. The pod + the seeds are...")
   end
   
+  BOT.command :bell_pepper do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 7
+    challenge.update(stage: 8)
+    
+    event.respond("Seeds that can be eaten, and are contained within a pod. The pod + the seeds are...")
+  end
+  
+  BOT.command :bellpeppers do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 7
+    challenge.update(stage: 8)
+    
+    event.respond("Seeds that can be eaten, and are contained within a pod. The pod + the seeds are...")
+  end
+  
+  BOT.command :bellpepper do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 7
+    challenge.update(stage: 8)
+    
+    event.respond("Seeds that can be eaten, and are contained within a pod. The pod + the seeds are...")
+  end
+  
   BOT.command :pea_pods do |event|
     break unless event.user.id.player?
 
@@ -138,7 +278,35 @@ class Sunny
     event.respond("Cylindrical to spherical, green vegetables, which are used as culinary vegetables, and the ones to attend the last Tribal Council.")
   end
   
+  BOT.command :pea_pod do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 8
+    challenge.update(stage: 9)
+    
+    event.respond("Cylindrical to spherical, green vegetables, which are used as culinary vegetables, and the ones to attend the last Tribal Council.")
+  end
+  
   BOT.command :cucumbers do |event|
+    break unless event.user.id.player?
+
+    player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
+    break unless Challenge.where(player_id: player.id).exists?
+    break unless [player.confessional, player.submissions].include? event.channel.id
+    challenge = player.challenges.first
+
+    break unless challenge.stage == 9
+    challenge.update(stage: 10)
+    
+    event.respond("A dish typically made of mixed vegetables, fruits, or grains, often served cold, and dressed with oil, vinegar, or sauces. It can include proteins like chicken, fish, or beans. Its main components can be Lettuces, Tomatoes, Carrots or Onions...!")
+  end
+  
+  BOT.command :cucumber do |event|
     break unless event.user.id.player?
 
     player = Player.find_by(user_id: event.user.id, season_id: Setting.last.season, status: ALIVE)
