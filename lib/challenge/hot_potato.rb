71c9377e45bff15f5994979a1b2919d18dec2b53
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-01-10 17:25:04 +0200

    Update hot_potato.rb

--- lib/challenge/hot_potato.rb
+++ lib/challenge/hot_potato.rb
@@ -2,7 +2,7 @@ class Sunny
   BOT.command :hot_potato do |event|
     break unless event.user.id.host?
 
-    break unless event.channel.id == 1327032751681175684
+    break unless event.channel.id == 1327288820529627146
 
     event.respond(":potato: **Hot Potato** has begun!\nAfter a not-so-random amount of time, the Potato will explode and take down the player holding it.")
     event.channel.start_typing
@@ -21,7 +21,7 @@ class Sunny
   BOT.command :pass do |event, *args|
     break unless event.user.id.player?
 
-    break unless event.channel.id == 1327032751681175684
+    break unless event.channel.id == 1327288820529627146
 
     players = Participant.where(status: 1).map { |player| Player.find_by(id: player.player_id) }
     passer = Player.find_by(user_id: event.user.id, status: ALIVE)
@@ -63,5 +63,9 @@ class Sunny
 
     Potato.all.first.update(player_id: matches.first.id)
     event.respond("The :potato: **Hot Potato** was passed to #{BOT.user(matches.first.user_id).mention}!")
+  end
+
+  BOT.command :explode do |event|
+    
   end
 end
