b3e712905ae4427bd8cc567c7a05c3ffdd775b03
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-05-07 09:56:59 +0200

    Update member_join.rb

--- lib/misc/member_join.rb
+++ lib/misc/member_join.rb
@@ -2,7 +2,7 @@ class Sunny
   BOT.member_join do |event|
     BOT.channel(USER_JOIN_CHANNEL).send_embed do |embed|
       embed.title = "#{event.user.name} has joined Alvivor! :tada: "
-      embed.description = "Welcome! We hope you enjoy your stay here.\nSeason 2 is just around the corner!"
+      embed.description = "Welcome! We hope you enjoy your stay here.\nHead over to <#1128055783519686756> if you wish to apply! And enjoy!"
       embed.color = '6944b9'
       embed.image = Discordrb::Webhooks::EmbedThumbnail.new(url: event.user.avatar_url)
     end
