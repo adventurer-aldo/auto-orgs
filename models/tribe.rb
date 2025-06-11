3cbd8096299cf321ae056e9aba6c78640d251606
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-06-11 04:11:24 +0200

    Update tribe.rb

--- models/tribe.rb
+++ models/tribe.rb
@@ -4,6 +4,7 @@ class Tribe < ActiveRecord::Base
   has_many :votes, through: :councils
   belongs_to :season, foreign_key: 'season_id'
   
+  has_many :challenges, foreign_key: 'tribe_id'
   # Battleships
   has_many :damages, foreign_key: 'tribe_id'
   has_many :battleships, foreign_key: 'tribe_id'
