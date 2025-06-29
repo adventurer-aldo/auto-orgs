935a917b3dd6f3cbc8dd97a1fedf9b2fa20486a3
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-06-29 18:35:04 +0200

    Update castlist.rb

--- lib/misc/castlist.rb
+++ lib/misc/castlist.rb
@@ -2,4 +2,9 @@ class Sunny
   BOT.command :cast do |event|
     event.respond("**Castaways currently in the game:**\n" + Player.where(status: ALIVE).pluck(:name).join("\n"))
   end
+
+  BOT.command :vote_count do |event|
+    new_council_votes = Council.last.votes.map(&:votes).flatten
+    event.respond("#{new_council_votes.size - new_council_votes.count(0)}/#{new_council_votes.size}")
+  end
 end
