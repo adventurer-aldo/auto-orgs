0c4721b2f7d3cb7ea97b3f700e371a70ebeb2805
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-01 16:39:48 +0200

    Update vote_reminder.rb

--- lib/jobs/vote_reminder.rb
+++ lib/jobs/vote_reminder.rb
@@ -6,7 +6,16 @@ class Sunny
 
       council = Council.find_by(id: council_id)
       council.votes.select { |vote| vote.votes.include?(0) }.map(&:player).each do |player|
-        BOT.channel(player.submissions).send_message("Reminder to vote #{BOT.user(player.user_id).mention}")
+        BOT.channel(player.submissions).send_message([
+          "Reminder to vote",
+          "Not much time left to submit a vote,",
+          "You haven't voted yet,",
+          "You forgot to vote",
+          "Just reminding you that you still haven't voted",
+          "Votes are being tallied not too late from now!",
+          "Hey, just reminding you..that you still haven't voted yet",
+          "Hmm... You know what's missing? Your vote"
+        ].sample + " #{BOT.user(player.user_id).mention}")
       end
       destroy
     end
