efdd953bb360dd04b7748155f3a0556b8d56ad17
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-07 19:52:24 +0200

    Update guess_who.rb

--- lib/challenge/guess_who.rb
+++ lib/challenge/guess_who.rb
@@ -16,7 +16,7 @@ class Sunny
     individual = Individual.create(player_id: player.id, stage: 0, start_time: Time.now.to_i)
     event.respond("The timer has begun! Guess who the following animal is and answer with a command!")
     file = URI.parse("https://seaworld.org/-/media/migrated-media/seaworld-dotorg/audio-files/sound-library/domestic_cat.wav?version=1_201811134819").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.ogg')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.ogg')
   end
 
   BOT.command :cat do |event|
@@ -33,7 +33,7 @@ class Sunny
     individual.update(stage: 1)
     event.respond("Correct! Next up...")
     file = URI.parse("https://seaworld.org/-/media/migrated-media/seaworld-dotorg/audio-files/sound-library/wolf.wav?version=1_201811134820").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.ogg')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.ogg')
   end
 
   BOT.command :wolf do |event|
@@ -50,7 +50,7 @@ class Sunny
     individual.update(stage: 2)
     event.respond("Correct! Next up...")
     file = URI.parse("https://seaworld.org/-/media/migrated-media/seaworld-dotorg/audio-files/sound-library/dogbark.wav?version=1_201811134824").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.ogg')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.ogg')
   end
 
   BOT.command :dog do |event|
@@ -67,7 +67,7 @@ class Sunny
     individual.update(stage: 3)
     event.respond("Correct! Next up...")
     file = URI.parse("https://www.animal-sounds.org/jungle/Elephant%20trumpeting%20animals129.wav").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.wav')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.wav')
   end
 
   BOT.command :elephant do |event|
@@ -84,7 +84,7 @@ class Sunny
     individual.update(stage: 4)
     event.respond("Correct! Next up...")
     file = URI.parse("https://www.animal-sounds.org/jungle/Monkey%20chatter%20animals059.wav").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.wav')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.wav')
   end
 
   BOT.command :monkey do |event|
@@ -101,7 +101,7 @@ class Sunny
     individual.update(stage: 5)
     event.respond("Correct! Next up...")
     file = URI.parse("https://seaworld.org/-/media/migrated-media/seaworld-dotorg/audio-files/sound-library/frog.wav?version=1_201811134824").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.wav')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.wav')
   end
 
   BOT.command :frog do |event|
@@ -118,7 +118,7 @@ class Sunny
     individual.update(stage: 6)
     event.respond("Correct! Next up...")
     file = URI.parse("https://www.animal-sounds.org/farm/Cow%20animals055.wav").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.wav')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.wav')
   end
 
   BOT.command :cow do |event|
@@ -135,7 +135,7 @@ class Sunny
     individual.update(stage: 7)
     event.respond("Correct! Next up...")
     file = URI.parse("https://seaworld.org/-/media/migrated-media/seaworld-dotorg/audio-files/sound-library/clydesdale.mp3?version=1_201811130506").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :bull do |event|
@@ -152,7 +152,7 @@ class Sunny
     individual.update(stage: 7)
     event.respond("Correct! Next up...")
     file = URI.parse("https://seaworld.org/-/media/migrated-media/seaworld-dotorg/audio-files/sound-library/clydesdale.mp3?version=1_201811130506").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :horse do |event|
@@ -169,7 +169,7 @@ class Sunny
     individual.update(stage: 8)
     event.respond("Correct! Next up...")
     file = URI.parse("https://seaworld.org/-/media/migrated-media/seaworld-dotorg/audio-files/sound-library/cougar.mp3?version=1_201811134819").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :panther do |event|
@@ -186,7 +186,7 @@ class Sunny
     individual.update(stage: 9)
     event.respond("Correct! Next up...")
     file = URI.parse("https://freeanimalsounds.org/wp-content/uploads/2017/08/mosquito.mp3").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :mosquito do |event|
@@ -203,7 +203,7 @@ class Sunny
     individual.update(stage: 10)
     event.respond("Correct! Next up...")
     file = URI.parse("https://freeanimalsounds.org/wp-content/uploads/2017/07/rattlesnake.mp3").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :snake do |event|
@@ -220,7 +220,7 @@ class Sunny
     individual.update(stage: 11)
     event.respond("Correct! Next up...")
     file = URI.parse("https://freeanimalsounds.org/wp-content/uploads/2017/07/wolf.mp3").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :rattlesnake do |event|
@@ -237,7 +237,7 @@ class Sunny
     individual.update(stage: 11)
     event.respond("Correct! Next up...")
     file = URI.parse("https://freeanimalsounds.org/wp-content/uploads/2017/07/wolf.mp3").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :wolf do |event|
@@ -254,7 +254,7 @@ class Sunny
     individual.update(stage: 12)
     event.respond("Correct! Next up...")
     file = URI.parse("https://seaworld.org/-/media/migrated-media/seaworld-dotorg/audio-files/sound-library/falcon.wav?version=1_201811134818").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.wav')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.wav')
   end
 
   BOT.command :falcon do |event|
@@ -271,7 +271,7 @@ class Sunny
     individual.update(stage: 13)
     event.respond("Correct! Next up...")
     file = URI.parse("https://freeanimalsounds.org/wp-content/uploads/2017/07/schafe.mp3").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :sheep do |event|
@@ -288,7 +288,7 @@ class Sunny
     individual.update(stage: 14)
     event.respond("Correct! Next up...")
     file = URI.parse("https://freeanimalsounds.org/wp-content/uploads/2017/07/Ente_quackt.mp3").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.mp3')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.mp3')
   end
 
   BOT.command :duck do |event|
@@ -305,7 +305,7 @@ class Sunny
     individual.update(stage: 15)
     event.respond("Correct! Next up...")
     file = URI.parse("https://cdn.discordapp.com/attachments/1378044547287879731/1391834767002959872/ringa.ogg?ex=686d56a3&is=686c0523&hm=da0373185e3759c810de9a34efb5370647ab6e799765e7b83b666acd6f5ef6b1&").open
-    BOT.send_file(BOT.channel(1378044547287879731), file, filename: 'mystery_animal.wav')
+    BOT.send_file(event.channel, file, filename: 'mystery_animal.wav')
   end
 
   BOT.command :human do |event|
