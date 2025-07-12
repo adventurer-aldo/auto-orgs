d60018bea19dc9100fb143376e8e15da7cd3a444
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-12 22:32:16 +0200

    Update test.rb

--- lib/misc/test.rb
+++ lib/misc/test.rb
@@ -25,11 +25,25 @@ class Sunny
       convert << image_file.path
     end
 
-    gamma = MiniMagick::Image.open(image_file.path)
+    gamma = MiniMagick::Image.open(image_file.path).combine_options do |c|
+      c.background 'none'
+      c.rotate(angle)
+      c.trim
+    end
+
+    if gamma.dimensions[0] > 1160
+      max_width = 1160.0
+      scale = [1.0, max_width / gamma.dimensions[0]].min
+      new_width = (gamma.dimensions[0] * scale).round
+      new_height = (gamma.dimensions[1] * scale).round
+
+      gamma = gamma.resize("#{new_width}x#{new_height}")
+    end
 
     result = base.composite(gamma) do |c|
       c.gravity "center"
     end
+    
     tmp = Tempfile.new(["output", ".png"])
     result.write(tmp.path)
     BOT.channel(HOST_CHAT).send_file(tmp)
