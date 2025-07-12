23e62a31aeef2e1651a31a58be6bb81c443c9746
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-12 04:51:57 +0200

    Update test.rb

--- lib/misc/test.rb
+++ lib/misc/test.rb
@@ -1,16 +1,32 @@
 class Sunny
   BOT.command :test do |event|
+    parch = Tempfile.new(["parchment", ".jpg"])
+    parch.write URI.parse(PARCHMENT).read
+    parch.rewind
+    
+    base = MiniMagick::Image.open(parch.path)
     image_file = Tempfile.new(["output", ".png"])
+    
     MiniMagick::Tool.new("convert") do |convert|
       convert.size "300x100"
-      convert.xc "white"
+      convert.xc "none"
       convert.fill "black"
       convert.gravity "center"
-      convert.pointsize 40
+      convert.pointsize 100
       convert.annotate "0", "Hayden"
       convert << image_file.path
     end
-    BOT.channel(HOST_CHAT).send_file(image_file)
+    gamma = MiniMagick::Image.open(image_file.path).rotate(30).combine_options do |c|
+      c.fuzz "80%"
+      c.transparent "white"
+    end
+
+    result = base.composite(gamma) do |c|
+      c.gravity "center"
+    end
+    tmp = Tempfile.new(["output", ".png"])
+    result.write(tmp.path)
+    BOT.channel(HOST_CHAT).send_file(tmp)
   end
 
 end
