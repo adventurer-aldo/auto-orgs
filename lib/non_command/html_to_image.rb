3ee383070b03715310334f1cd7129e4283aa1e7d
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-13 16:34:58 +0200

    Update html_to_image.rb

--- lib/non_command/html_to_image.rb
+++ lib/non_command/html_to_image.rb
@@ -33,17 +33,7 @@ class Sunny
     white-space: nowrap;
   }
     </style>
-  <table class=\"container table table-striped table-sm table-secondary table-bordered border-dark \">
-    <thead class=\"table-dark\">
-      <tr>
-        <th scope=\"col\">Spectators</th><th>Winner Pick</th><th>Pick 1</th><th>Pick 2</th><th>Pick 3</th>
-      </tr>
-    </thead>
-    <tbody class=\"table-group-divider\">
-      <tr><td>Ana</td><td>Yes</td><td>Leo</td><td>Mira</td><td>Jude</td></tr>
-      <tr><td>Ben</td><td>No</td><td>Ken</td><td>Aria</td><td>Leo</td></tr>
-    </tbody>
-  </table>")
+  #{string}")
     request.body = JSON.dump({
       "page": {
         "screenshot": {
