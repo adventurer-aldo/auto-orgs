cf522b0ed90953d7cf62e53ca539f5b7d03b77c0
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-13 03:18:05 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -1,6 +1,6 @@
 class Sunny
   BOT.ready do
-    BOT.send_message(HOST_CHAT, 'Hello, world!')
+    BOT.send_message(HOST_CHAT, 'Hello, new world!')
     BOT.game = 'Alvivor Season 2!'
     # row.button(style: Discordrb::Webhooks::View::BUTTON_STYLES[:success], label: "Begin Application!", custom_id: 'application_start_button')
     # Que.migrate!(version: 7)
