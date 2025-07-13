c047dc1d742c37f85ff4b7f6f78d89ec4ce519ec
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-13 16:58:30 +0200

    Update test.rb

--- lib/misc/test.rb
+++ lib/misc/test.rb
@@ -1,7 +1,6 @@
 class Sunny
 
   BOT.command :to_html do |event, *args|
-    event.respond(args.join(' '))
     event.channel.send_file(html_to_image("<div style=\"
     background-color: red;
     color: white;
