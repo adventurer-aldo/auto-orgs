bff50502c320e5b657d4d63d60ef2ec84debd070
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-13 16:37:49 +0200

    Update test.rb

--- lib/misc/test.rb
+++ lib/misc/test.rb
@@ -2,6 +2,6 @@ class Sunny
 
   BOT.command :to_html do |event, *args|
     event.respond(args.join(''))
-    event.channel.send_file(html_to_image("args.join('')"), filename: 'image.png')
+    event.channel.send_file(html_to_image(args.join(' ')), filename: 'image.png')
   end
 end
