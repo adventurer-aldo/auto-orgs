81ec56605ef40a7dfcb869b9d55b73636edb26ed
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-13 19:16:44 +0200

    Update test.rb

--- lib/misc/test.rb
+++ lib/misc/test.rb
@@ -1,18 +1,30 @@
 class Sunny
 
-  BOT.command :to_html do |event, *args|
-    event.channel.send_file(html_to_image("<div style=\"
-    background-color: red;
-    color: white;
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    text-align: center;
-    font-family: Arial, sans-serif;
-    font-size: 18px;
-    font-weight: bold;
-\">
-    We got this going good
-</div>"), filename: 'image.png')
+  BOT.command :draft do |event|
+    base = %Q(
+  <table class="table table-secondary table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Spectator</th>
+      <th scope="col">Winner Pick</th>
+      <th scope="col">Pick 1</th>
+      <th scope="col">Pick 2</th>
+      <th scope="col">Pick 3</th>
+    </tr>
+  </thead>
+  <tbody>
+  #{SpectatorGames::Draft.where(season_id: Setting.last.season).map do |draft|
+%Q(
+    <tr>
+      <th scope="row">#{BOT.user(draft.user_id).on(ALVIVOR_ID).display_name}</th>
+      <td>#{Player.find_by(id: draft.winner_pick)}</td>
+      <td>#{Player.find_by(id: draft.pick_1)}</td>
+      <td>#{Player.find_by(id: draft.pick_2)}</td>
+      <td>#{Player.find_by(id: draft.pick_3)}</td>
+    </tr>)
+  end.join('')}
+  </tbody>
+</table>)
+    event.channel.send_file(html_to_image(base), filename: 'image.png')
   end
 end
