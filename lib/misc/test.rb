8f5e9ce7161c22a1ce02b725927fb319418ae902
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-13 16:55:44 +0200

    Update test.rb

--- lib/misc/test.rb
+++ lib/misc/test.rb
@@ -2,6 +2,18 @@ class Sunny
 
   BOT.command :to_html do |event, *args|
     event.respond(args.join(' '))
-    event.channel.send_file(html_to_image(args.join(' ')), filename: 'image.png')
+    event.channel.send_file(html_to_image("<div style=\"
+    background-color: red;
+    color: white;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    font-size: 18px;
+    font-weight: bold;
+\">
+    We got this going good
+</div>"), filename: 'image.png')
   end
 end
