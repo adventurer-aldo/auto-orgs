0fa6e5e843e47a317df839152a42ff0dbae96fa0
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-19 12:20:17 +0200

    Update run.rb

--- run.rb
+++ run.rb
@@ -38,11 +38,19 @@ require_relative 'settings'
 require_all 'models'
 require_all 'lib'
 
+# Initialize Storage system
 Shrine.plugin :determine_mime_type, analyzer: :file
+
+auth_str = Base64.strict_encode64("#{ENV['AWS_ACCESS_KEY_ID']}:#{ENV['AWS_SECRET_ACCESS_KEY']}")
+uri = URI("https://api.backblazeb2.com/b2api/v2/b2_authorize_account")
+req = Net::HTTP::Get.new(uri)
+req["Authorization"] = "Basic #{auth_str}"
+res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) { |http| http.request(req) }
+data = JSON.parse(res.body)
 
 Shrine.storages = {
   store: Shrine::Storage::B2Native.new(
-    bucket_id: ENV['AWS_BUCKET_ID'], auth_token: ENV['AWS_TOKEN'], api_url: ENV['AWS_API_URL'], bucket_name: ENV['AWS_BUCKET_NAME'], region: ENV['AWS_REGION']
+    bucket_id: ENV['AWS_BUCKET_ID'], auth_token: data["authorizationToken"], api_url: ENV['AWS_API_URL'], bucket_name: ENV['AWS_BUCKET_NAME'], region: ENV['AWS_REGION']
   )
 }
 
