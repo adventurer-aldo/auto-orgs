9b8d521756cca260c1465cb8360e0ee6b8413faa
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-26 16:03:02 +0200

    Update join/leave messages and clean up ready event
    Changed user references from name to display_name in join/leave events for better display. Updated the welcome message to reflect the end of Season 2 and removed outdated comments from the ready event.

--- lib/misc/member_join.rb
+++ lib/misc/member_join.rb
@@ -1,8 +1,8 @@
 class Sunny
   BOT.member_join do |event|
     BOT.channel(USER_JOIN_CHANNEL).send_embed do |embed|
-      embed.title = "#{event.user.name} has joined Alvivor! :tada: "
-      embed.description = "Welcome! We hope you enjoy your stay here.\nHead over to <#1128055783519686756> if you wish to apply! And enjoy!"
+      embed.title = "#{event.user.display_name} has joined Alvivor! :tada: "
+      embed.description = "Welcome! **Season 2: Animals** just ended, but we hope you stick around while our next season is in the works!"
       embed.color = '6944b9'
       embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: event.user.avatar_url)
     end
@@ -10,6 +10,6 @@ class Sunny
   end
 
   BOT.member_leave do |event|
-    BOT.channel(USER_LEAVE_CHANNEL).send_message("**#{event.user.name}** has just left the jungle...")
+    BOT.channel(USER_LEAVE_CHANNEL).send_message("**#{event.user.display_name}** has just left...")
   end
 end
--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -2,9 +2,5 @@ class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, 'Hello, new world!')
     BOT.game = 'with preparations for a new season!'
-    # row.button(style: Discordrb::Webhooks::View::BUTTON_STYLES[:success], label: "Begin Application!", custom_id: 'application_start_button')
-    # Que.migrate!(version: 7)
-    # TestJob.enqueue(job_options: { run_at: Time.now + 300})
-    # S3.upload(File.open("audio/elim.wav"), "new_elim")
   end
 end
