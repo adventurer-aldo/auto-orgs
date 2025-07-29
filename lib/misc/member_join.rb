90c1ad5fc82901947443f60259be00582423cf54
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-29 11:23:18 +0200

    Update member_join.rb

--- lib/misc/member_join.rb
+++ lib/misc/member_join.rb
@@ -2,14 +2,14 @@ class Sunny
   BOT.member_join do |event|
     BOT.channel(USER_JOIN_CHANNEL).send_embed do |embed|
       embed.title = "#{event.user.display_name} has joined Alvivor! :tada: "
-      embed.description = "Welcome! **Season 2: Animals** just ended, but we hope you stick around while our next season is in the works!"
-      embed.color = '6944b9'
+      embed.description = "Welcome! **Season 3: Spirits & Souls**'s applications are soon to be up!"
+      embed.color = '9a5cd8'
       embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: event.user.avatar_url)
     end
     event.user.on(event.server).add_role(1113168262461673532)
   end
 
   BOT.member_leave do |event|
-    BOT.channel(USER_LEAVE_CHANNEL).send_message("**#{event.user.display_name}** has just left...")
+    BOT.channel(USER_LEAVE_CHANNEL).send_message("**#{event.user.display_name}**'s spirit has left us...")
   end
 end
