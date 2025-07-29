d6c43c43404de9d19659699aa1e42b9116f7b887
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-29 22:43:21 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -2,5 +2,8 @@ class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, 'Hello, new world!')
     BOT.game = 'with preparations for a new season!'
+    view = Discordrb::Webhooks::View.new
+    view.row { |row| row.string_select(custom_id: "WinnerPick", options: Player.all.map { |player| { label: player.name, value: player.id }})}
+    BOT.channel(HOST_CHAT).send_message("Choose your Winner Pick!", false, nil, nil, nil, nil, view)
   end
 end
