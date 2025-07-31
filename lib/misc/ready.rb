4614639fb747d314bc5ff44251dace7f3008f6d6
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-07-31 10:53:46 +0200

    s

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -1,7 +1,7 @@
 class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, new world! <a:torch:1400359863393062952>')
-    BOT.game = 'with preparations for a new season!'
+    BOT.game = 'Season 3: Spirits & Souls!'
     view = Discordrb::Webhooks::View.new
     view.button(custom_id: "application_start_button", label: "Start Application", style: 3)
     BOT.channel(HOST_CHAT).send_message("**Applications for Alvivor Season 3: Spirits & Souls are up!**\n\nAlumni (for a maximum of 6) and newbies are allowed to apply. \nClick the button below to begin your application!", false, nil, nil, nil, nil, view)
