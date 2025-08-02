29c95136442bda9207a5cf0a8893d01a5ed088c7
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-02 11:39:35 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -2,9 +2,6 @@ class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, new world! <a:torch:1400359863393062952>')
     BOT.game = 'Season 3: Spirits & Souls!'
-    view = Discordrb::Webhooks::View.new()
-    view.row { |row| row.string_select(custom_id: "EliminationPick", options: [{label: 'No One', value: '0'}]) }
-    BOT.channel(1393731026882269398).send_message("Choose a castaway currently in the game **that you haven't picked before**.", false, nil, nil, nil, nil, view)
   end
 
   BOT.string_select(custom_id: "WinnerPick") do |event|
