8337b2e1d072584e2d80f21df96f81670b00ecaa
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-25 22:36:06 +0200

    Update stats.rb

--- lib/misc/stats.rb
+++ lib/misc/stats.rb
@@ -2,6 +2,8 @@ class Sunny
 
   BOT.command :season_timer do |event|
     break unless event.user.id.host?
+    a = Season.last
+    a.update(start_time: Time.now)
     InServerStats.enqueue(job_options: {run_at: Time.now})
   end
 
