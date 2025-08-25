30d51fdbe71bbbfdf15c128b3a85db4a6584b7eb
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-25 22:41:06 +0200

    Update inserver_stats.rb

--- lib/jobs/inserver_stats.rb
+++ lib/jobs/inserver_stats.rb
@@ -6,7 +6,7 @@ class Sunny
     def run
       season = Setting.last.season
       player_size = Player.where(status: ALIVE, season:).size
-      BOT.channel(1388974050717732895).name = "Season #{season} - Day #{((Time.now.to_i - Season.last.start_time.to_i) / 60 / 60 / 24)} - F#{player_size}"
+      BOT.channel(1388974050717732895).name = "Season #{season} - Day #{((Time.now.to_i - Season.last.start_time.to_i) / 60 / 60 / 24) + 1} - F#{player_size}"
 
       InServerStats.enqueue if player_size > 3
       destroy
