2e9f754970b625d94a62244c553bd069148944cb
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-26 18:09:02 +0200

    Update wordle.rb

--- lib/challenge/wordle.rb
+++ lib/challenge/wordle.rb
@@ -8,7 +8,7 @@ class Sunny
       event.respond "Yeah that word exists in wordle"
     else
       event.respond "Nah, that word ain't exist in wordle"
-      event.respond word
+      event.respond [word, @wordle[3]].to_s
       event.respond @wordle[3] == word
     end
   end
