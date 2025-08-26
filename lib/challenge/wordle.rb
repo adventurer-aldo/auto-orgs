6497dcc2d66bfad3eb3b521de2de31ff5bf7b68d
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-26 18:05:29 +0200

    Update wordle.rb

--- lib/challenge/wordle.rb
+++ lib/challenge/wordle.rb
@@ -1,13 +1,15 @@
 class Sunny
 
-  WORDLE = File.open('./lib/create/merge_cheers.txt', 'r').readlines
+  @wordle = File.open('./lib/create/merge_cheers.txt', 'r').readlines
 
   BOT.command :word do |event, *args|
     word = args.join('').downcase
-    if WORDLE.include? word
+    if @wordle.include? word
       event.respond "Yeah that word exists in wordle"
     else
       event.respond "Nah, that word ain't exist in wordle"
+      event.respond word
+      event.respond @wordle[3]
     end
   end
 end
