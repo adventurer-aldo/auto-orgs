da0d90a9f383a5d9584379ce3f36bb86454dbec2
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-27 01:20:12 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -3,17 +3,4 @@ class Sunny
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, new world! <a:torch:1400359863393062952>')
     BOT.game = 'Season 3: Spirits & Souls!'
   end
-
-  BOT.string_select(custom_id: "WinnerPick") do |event|
-    event.defer_update
-
-    event.channel.send_message("You chose **#{Player.find_by(id: event.values.first.to_i).name}**")
-    draft = SpectatorGame::Draft.find_or_create_by(user_id: event.user.id, season_id: 2)
-    draft.update(winner_pick: event.values.first.to_i)
-  end
-
-  BOT.string_select(custom_id: 'EliminationPick') do |event|
-    event.defer_update
-    event.send_message(content: "That's not a valid pick!", ephemeral: true)
-  end
 end
