aee80df535ddd17611352e75116095c18712e26f
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-28 04:11:03 +0200

    Update alliance.rb

--- models/alliances/alliance.rb
+++ models/alliances/alliance.rb
@@ -1,6 +1,6 @@
 module Alliances
   class Group < ActiveRecord::Base
-    has_many :alliance_associatons, class_name: 'Alliances::Association', dependent: :destroy
+    has_many :alliance_associatons, foreign_key: 'alliance_id', class_name: 'Alliances::Association', dependent: :destroy
     has_many :players, through: :alliance_associatons
   end
 end
