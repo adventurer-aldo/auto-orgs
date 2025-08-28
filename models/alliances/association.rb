2ae41a04909a2d96bd69c6e09667f07b9cb62451
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-28 04:11:38 +0200

    Update association.rb

--- models/alliances/association.rb
+++ models/alliances/association.rb
@@ -1,5 +1,5 @@
 module Alliances
   class Association < ActiveRecord::Base
-    belongs_to :alliance, class_name: 'Alliances::Group', foreign_key: 'player_id'
+    belongs_to :alliance, class_name: 'Alliances::Group', foreign_key: 'alliance_id'
   end
 end
