b26d63dc4d233843846d1ea3bc75311fb28c7478
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-29 04:31:42 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -2,10 +2,10 @@ class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, new world! <a:torch:1400359863393062952>')
     BOT.game = 'Season 3: Spirits & Souls!'
-    
+    BOT.channel(HOST_CHAT).send_file(URI.parse('https://cdn.discordapp.com/attachments/1409633533101736047/1410439407676162108/parchment-35.png?ex=68b2570f&is=68b1058f&hm=ced9370e45d120678526b1bf37f4c8d7ba2abc607aa2e39ba0f6191fed9817a4').open, filename: 'Hey.png')
   end
 
   BOT.message(in: HOST_CHAT, from: 460766095188688903) do |event|
-    event.channel.send_file(URI.parse('https://cdn.discordapp.com/attachments/1409633533101736047/1410439407676162108/parchment-35.png?ex=68b2570f&is=68b1058f&hm=ced9370e45d120678526b1bf37f4c8d7ba2abc607aa2e39ba0f6191fed9817a4'), filename: 'Hey.png')
+    event.channel.send_file(URI.parse('https://cdn.discordapp.com/attachments/1409633533101736047/1410439407676162108/parchment-35.png?ex=68b2570f&is=68b1058f&hm=ced9370e45d120678526b1bf37f4c8d7ba2abc607aa2e39ba0f6191fed9817a4').open, filename: 'Hey.png')
   end
 end
