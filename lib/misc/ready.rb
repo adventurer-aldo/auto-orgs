529e2e04152025b44c9076bef72aad0f436b7ab6
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-29 04:24:51 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -6,6 +6,8 @@ class Sunny
   end
 
   BOT.message(in: HOST_CHAT, from: 460766095188688903) do |event|
-    BOT.channel(HOST_CHAT).send_message(event.message.content, false, nil, event.message.attachments.map { |file| URI.parse(file.url)})
+    event.respond event.message.attachments
+    event.respond event.message.attachments.map(&:url)
+    BOT.channel(HOST_CHAT).send_message(event.message.content, false, nil, event.message.attachments.map { |attachment| URI.parse(attachment.url)})
   end
 end
