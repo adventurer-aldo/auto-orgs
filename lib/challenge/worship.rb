9564fc56610ce8156f2af39ad033b4b73f80bc4f
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-08-31 20:37:46 +0200

    Create worship.rb

---
+++ lib/challenge/worship.rb
@@ -0,0 +1,41 @@
+class Sunny
+  Tribe.where(id: Setting.last.tribes).pluck(:cchannel_id).each do |channel_id|
+    BOT.message(in: channel_id) do |event|
+      break unless event.user.id.player?
+      
+      player = Player.find_by(user_id: event.user.id, season: Setting.last.season)
+      tribe = player.tribe
+      challenge = Challenges::Tribal.find_by(tribe_id: tribe.id)
+      
+      correct = false
+      if tribe.id == 25 # Uada
+        case challenge.stage
+        when 0
+          if event.message.content == 'I worship Idan.'
+            challenge.update(end_time: challenge.end_time + 1, stage: 1) 
+            correct = true
+          end
+        when 1
+          if event.message.content == 'Idan lives on within us.'
+            challenge.update(end_time: challenge.end_time + 1, stage: 0) 
+            correct = true
+          end
+        end
+      elsif tribe.id == 26 # Habiti
+        case challenge.stage
+        when 0
+          if event.message.content == 'I worship Isaiah.'
+            challenge.update(end_time: challenge.end_time + 1, stage: 1) 
+            correct = true
+          end
+        when 1
+          if event.message.content == 'May they be reborn anew.'
+            challenge.update(end_time: challenge.end_time + 1, stage: 0) 
+            correct = true
+          end
+        end
+      end
+      event.message.create_reaction('✅') if correct == true
+    end
+  end
+end
