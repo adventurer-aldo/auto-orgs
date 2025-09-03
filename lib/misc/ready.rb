38340e7bd1a5ec62a3bc1e5630d2ed8879126eb2
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-09-03 17:07:35 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -2,6 +2,5 @@ class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, new world! <a:torch:1400359863393062952>')
     BOT.game = 'Season 3: Spirits & Souls!'
-    BOT.channel(1409633515259039765).send_message("A spirit sends its regards...\n🔥***Stew* is rooting for you to win the game!**")
   end
 end
