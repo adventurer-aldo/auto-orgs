8b237af60ac17224fb8068239e705968a84fd1b1
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-09-03 17:06:45 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -2,7 +2,6 @@ class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, new world! <a:torch:1400359863393062952>')
     BOT.game = 'Season 3: Spirits & Souls!'
-    InServerStats.enqueue(job_options: {run_at: Time.now})
-    WorshipUpdateJob.enqueue(job_options: {run_at: Time.now})
+    BOT.channel(1409633515259039765).send_message("A spirit sends its regards...\n🔥***Stew* is rooting for you to win the game!**")
   end
 end
