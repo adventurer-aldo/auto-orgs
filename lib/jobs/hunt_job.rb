29b8550a13b21f7c31564d02576d3c66071f759b
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-09-05 11:45:37 +0200

    Create hunt_job.rb

---
+++ lib/jobs/hunt_job.rb
@@ -0,0 +1,12 @@
+class Sunny
+  class HuntJob < Que::Job
+    self.run_at = proc { Time.now + 3 * 60 * 60 } # 10 minutes from now
+
+    def run
+      # Call your results reveal/update method
+      Sunny.reveal_results_and_update if Challenge::Individual.where(start_time: nil).where.not(stage: 0).exists?
+
+      destroy
+    end
+  end
+end
