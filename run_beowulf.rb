ec86c27d1740605650599c33e5f596c250f7b106
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-09-06 21:43:47 +0200

    Update run_beowulf.rb

--- run_beowulf.rb
+++ run_beowulf.rb
@@ -16,4 +16,23 @@ class Beowulf
   end
 end
 
-Beowulf.run
+@tokens = {
+  stew: ENV['1'],
+  lynn: ENV['2'],
+  isaiah: ENV['3'],
+  idan: ENV['4'],
+  emerald: ENV['5'],
+  tabi: ENV['6']
+}
+
+@tokens.keys.each do |key|
+  bot = Discordrb::Commands::CommandBot.new token: @tokens[key], prefix: '?'
+  
+  bot.ready do |event|
+    bot.game = "Alvivor S3: Spirits & Souls"
+  end
+
+  bot.run false
+end
+
+# Beowulf.run
