6f99f583c1d8c9af779129063ad927d31749e98a
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-09-09 16:43:01 +0200

    Update item.rb

--- lib/create/item.rb
+++ lib/create/item.rb
@@ -2,6 +2,10 @@ class Sunny
   # > Item
   # > Council
 
+  BOT.command :get_items do |event|
+    Item.all.map { |item|  "#{item.name}, owned by #{[0, nil].include?(item.player_id) ? 'no one' : item.player.name } — Code: `#{item.code}`"}
+  end
+
   BOT.command :item, description: "Creates a new item to be claimed." do |event, *args|
     break unless HOSTS.include? event.user.id
 
