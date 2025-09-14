588c2769b51ec50b806c784b2cc14a9b81e3f00b
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-09-14 20:50:36 +0200

    Update setting.rb

--- models/setting.rb
+++ models/setting.rb
@@ -3,6 +3,10 @@ class Setting < ActiveRecord::Base
     return find_by(name: 'game_stage').values.first.to_i
   end
 
+  def self.game_stage=(value)
+    find_by(name: 'game_stage').update(values: [value])
+  end
+
   def self.season_id
     return find_by(name: 'season_id').values.first.to_i
   end
@@ -11,25 +15,24 @@ class Setting < ActiveRecord::Base
     return season_id
   end
 
+  def self.season_id=(value)
+    find_by(name: 'season_id').update(values: [value])
+  end
+
   def self.archive_category
     return find_by(name: 'archive_category').values.first.to_i
+  end
+
+  def self.archive_category=(value)
+    find_by(name: 'archive_category').update(values: [value])
   end
 
   def self.tribes
     return find_by(name: 'tribes').values.map(&:to_i)
   end
 
-  def self.game_stage=(value)
-    find_by(name: 'game_stage').update(values: [value])
-  end
-
   def self.tribes=(value)
     find_by(name: 'tribes').update(values:)
   end
-
-  def self.archive_category=(value)
-    find_by(name: 'archive_category').update(values:)
-  end
-
 
 end
