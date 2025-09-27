b98a4f3ed3059a6a9a4bafdd27f58216d74d8ede
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-09-27 23:25:40 +0200

    Update potato_job.rb

--- lib/jobs/potato_job.rb
+++ lib/jobs/potato_job.rb
@@ -1,6 +1,6 @@
 class Sunny
   class PotatoJob < Que::Job
-    self.run_at = proc { Time.now + (144 * 60) } # 2 hours 11 minutes from now
+    self.run_at = proc { Time.now + (8 * 60 * 60) } # 2 hours 11 minutes from now
 
     def run
       # Call your results reveal/update method
