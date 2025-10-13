55b9c80dc67118716e3fc0d0e99eb849ab045bde
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-10-13 12:02:39 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -1,6 +1,6 @@
 class Sunny
   BOT.ready do
-    BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, new world! <a:torch:1400359863393062952>')
+    BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, world! <a:torch:1400359863393062952>')
     BOT.game = 'Season 4!'
     # BOT.send_message(HOST_CHAT, QueJob.all)
     make_item_commands
