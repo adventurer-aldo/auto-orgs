01d0852a4115326c26452579522fcf10d28bfeb6
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-10-13 12:34:49 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -2,7 +2,10 @@ class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, world! <a:torch:1400359863393062952>')
     BOT.game = 'Season 4!'
-    # BOT.send_message(HOST_CHAT, QueJob.all)
+    council_and_votes = Setting.season.councils.map { |council| council.votes.map { |vote| "**#{vote.player.name}** voted #{votes.map { |vote_id| Player.find_by(id: vote_id).name}.join(', ')}"}.join("\n")}
+    council_and_votes.each do |council_votes|
+      BOT.send_message(HOST_CHAT, council_votes)
+    end
     make_item_commands
 
     # Add Ons
