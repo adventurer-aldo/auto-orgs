0c863d86589ca440dfa14000b917bf0fcf1039cb
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-10-29 17:42:22 +0200

    next

--- lib/misc/member_join.rb
+++ lib/misc/member_join.rb
@@ -1,7 +1,7 @@
 class Sunny
   BOT.member_join do |event|
     BOT.channel(USER_JOIN_CHANNEL).send_embed do |embed|
-      embed.title = "#{event.user.display_name} has joined Alvivor! :tada: "
+      embed.title = "#{event.user.display_name}'s time here has begun! :arrow_forward:"
       embed.description = "Welcome! We're glad to have you here!\n**Season 4**'s applications are on the horizon..."
       embed.color = 'ffdf15'
       embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: event.user.avatar_url)
@@ -10,6 +10,6 @@ class Sunny
   end
 
   BOT.member_leave do |event|
-    BOT.channel(USER_LEAVE_CHANNEL).send_message(":pause_button: **#{event.user.display_name}**'s time on this server has paused...")
+    BOT.channel(USER_LEAVE_CHANNEL).send_message(":pause_button: **#{event.user.display_name}**'s time here has paused...")
   end
 end
--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -1,21 +1,7 @@
 class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello, our world! <a:torch:1400359863393062952>')
-    BOT.game = 'Season 4!'
-    council_and_votes = Setting.season.councils.map do |council| 
-      council.votes.map do |vote| 
-        names = vote.votes.map do |vote_id| 
-          vote_id == 0 ? "nobody" : Player.find_by(id: vote_id).name
-        end.join(', ')
-        "**#{vote.player.name}** voted #{names}"
-      end.join("\n")
-    end
-    council_and_votes.each_with_index do |council_votes, index|
-      BOT.send_message(HOST_CHAT, "# Tribal Council No. #{index + 1}")
-      
-      BOT.send_message(HOST_CHAT, council_votes)
-      sleep(30)
-    end
+    BOT.game = 'Alvivor Season 4: ???!'
     make_item_commands
 
     # Add Ons
