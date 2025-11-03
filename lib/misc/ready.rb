90ec13a292b5a8508605c7247da148c8d6bfcf16
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-11-03 13:51:44 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -7,7 +7,7 @@ class Sunny
     test = BOT.channel(1434870641156423832)
     conn = BOT.voice_connect(test)
     sleep(5)
-    conn.play_io(URI.parse("https://cdn.discordapp.com/attachments/1378044547287879731/1434872190402170960/Tally_the_Vote_-_Game_Changers_5DSrD4yt2Ec.opus?ex=6909e85c&is=690896dc&hm=6beb804b95cc7718fd273a1c064095249fed5d4551158bc4dd2a6bde6571719e&").open)
+    conn.play_io(URI.parse("https://cdn.discordapp.com/attachments/1378044547287879731/1434872609996275733/stag.ogg?ex=6909e8c0&is=69089740&hm=53ed0b1adb2234b62bfef3b9e39993e39c9c5a9e8f8f9d32163fa5e7a4491617&").open)
     # Add Ons
    # hayden = Player.find_by(user_id: 198321560153489408, season_id: 2)
    # Search.where(player_id: hayden.id).update(player_id: Player.find_by(user_id: 198321560153489408, season_id: 3).id)
