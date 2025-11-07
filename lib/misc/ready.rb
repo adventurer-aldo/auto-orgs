6b7f61b472f8d52a8d41ca5c18ed8afa562b287c
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-11-07 23:53:03 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -9,7 +9,7 @@ class Sunny
     test = BOT.channel(1434870641156423832)
     conn = BOT.voice_connect(test)
     puts("Yeah. We go.")
-    url = "https://cdn.discordapp.com/attachments/1378044547287879731/1436350018448658634/output.wav?ex=690f48b2&is=690df732&hm=257c2b54d5c06b7835167469dbf0dc9f82fe4d3585c382e9949a629ba4498bcc&"
-    conn.play_file(url)
+    url = "https://cdn.discordapp.com/attachments/1378044547287879731/1436473483688284170/output.dca?ex=690fbbae&is=690e6a2e&hm=f8edaf462920a310054f059cd1760774844533ad0af78db27840932c33d444c6&"
+    conn.play_dca(url)
   end
 end
