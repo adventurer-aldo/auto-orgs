dbade2f0541611366acd9a75b0da94a1b89f3466
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-11-07 23:53:56 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -10,6 +10,6 @@ class Sunny
     conn = BOT.voice_connect(test)
     puts("Yeah. We go.")
     url = "https://cdn.discordapp.com/attachments/1378044547287879731/1436473483688284170/output.dca?ex=690fbbae&is=690e6a2e&hm=f8edaf462920a310054f059cd1760774844533ad0af78db27840932c33d444c6&"
-    conn.play_dca(url)
+    conn.play_dca(URI.parse(url).open)
   end
 end
