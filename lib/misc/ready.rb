7b0759c5a68e964f8fa49f16918d453779a665f8
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-11-07 23:12:31 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -8,7 +8,8 @@ class Sunny
     # Search.where(player_id: hayden.id).update(player_id: Player.find_by(user_id: 198321560153489408, season_id: 3).id)
     test = BOT.channel(1434870641156423832)
     conn = BOT.voice_connect(test)
-    puts("Yeah.")
-    conn.play_file("https://cdn.discordapp.com/attachments/1378044547287879731/1436314774622310441/tally.ogg?ex=690f27df&is=690dd65f&hm=b209d8878d0377d0f72839398787c0191e375fb666e48265451e40151bf82f7b&")
+    puts("Yeah. We go.")
+    url = "https://cdn.discordapp.com/attachments/1378044547287879731/1436350018448658634/output.wav?ex=690f48b2&is=690df732&hm=257c2b54d5c06b7835167469dbf0dc9f82fe4d3585c382e9949a629ba4498bcc&"
+    conn.play_file(url)
   end
 end
