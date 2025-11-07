1b84b4ca9c264556277ec3db1825ee4d7b2fba90
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-11-07 14:19:15 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -2,7 +2,7 @@ class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello! ....Our musical world? <a:torch:1400359863393062952>')
     # BOT.game = 'Alvivor Season 4:'
-    make_item_commands
+    #make_item_commands
     # Add Ons
    # hayden = Player.find_by(user_id: 198321560153489408, season_id: 2)
    # Search.where(player_id: hayden.id).update(player_id: Player.find_by(user_id: 198321560153489408, season_id: 3).id)
