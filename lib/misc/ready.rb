42b7ba047fbaa532fc82d6e449bffcc1dbe0dd0e
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-11-07 23:09:27 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -9,6 +9,6 @@ class Sunny
    test = BOT.channel(1434870641156423832)
    conn = BOT.voice_connect(test)
    puts("Yeah.")
-   conn.play_dca(URI.parse("https://github.com/shardlab/discordrb/raw/refs/heads/main/examples/data/music.dca").open)
+   conn.play_file("https://cdn.discordapp.com/attachments/1378044547287879731/1436314774622310441/tally.ogg?ex=690f27df&is=690dd65f&hm=b209d8878d0377d0f72839398787c0191e375fb666e48265451e40151bf82f7b&")
   end
 end
