e53ae7125188cd4ed46c5dfe1ffb027f41837ee3
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2025-11-10 11:11:26 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -25,8 +25,8 @@ class Sunny
     test = BOT.channel(1434870641156423832)
     conn = BOT.voice_connect(test)
     puts("Yeah. We go.")
-    url = "https://cdn.discordapp.com/attachments/1378044547287879731/1436473483688284170/output.dca?ex=690fbbae&is=690e6a2e&hm=f8edaf462920a310054f059cd1760774844533ad0af78db27840932c33d444c6&"
-    conn.play_dca(URI.parse(url).open)
+    file = Shrine.storages[:store].open("tally_test.dca")
+    conn.play_dca(file)
     BOT.send_message(HOST_CHAT, "Looks like it's time to boil up.")
   end
 end
