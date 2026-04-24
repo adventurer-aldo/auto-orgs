376d21951418a2d749a4f0199cbaaf13071feb49
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2026-04-24 21:18:13 +0200

    Update ready.rb

--- lib/misc/ready.rb
+++ lib/misc/ready.rb
@@ -1,7 +1,7 @@
 class Sunny
   BOT.ready do
     BOT.send_message(HOST_CHAT, '# <a:torch:1400359863393062952> Hello! Our fruity world! <a:torch:1400359863393062952>')
-    BOT.game = 'Alvivor Season 4: Fruits'
+    BOT.game = 'Resting'
     #make_item_commands
     # Add Ons
     # hayden = Player.find_by(user_id: 198321560153489408, season_id: 2)
