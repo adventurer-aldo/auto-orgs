9df5513e24e1e56714e59f5a90ba23ba875f8db8
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2026-04-28 21:30:20 +0200

    Update hosts.rb

--- lib/admin/hosts.rb
+++ lib/admin/hosts.rb
@@ -5,6 +5,22 @@ class Sunny
     ids.uniq
   end
 
+  def self.host_name_matches(query)
+    query = query.to_s.downcase
+    return [] if query == ''
+
+    Setting.hosts_ids.select do |id|
+      user = BOT.user(id)
+      names = [user&.username, user&.display_name, user&.name].compact.map(&:downcase)
+      names.any? { |name| name.include?(query) }
+    end
+  end
+
+  def self.host_display_name(id)
+    user = BOT.user(id)
+    user ? user.display_name : "Missing user #{id}"
+  end
+
   BOT.command :add_host do |event, *args|
     break unless event.user.id.host?
 
@@ -19,9 +35,16 @@ class Sunny
     break unless event.user.id.host?
 
     ids = host_ids_from_event(event, args)
-    return event.respond('Mention at least one user or provide at least one user ID.') if ids.empty?
+    if ids.empty?
+      matches = host_name_matches(args.join(' '))
+      return event.respond('Mention a host, provide a user ID, or write part of their username/display name.') if args.empty?
+      return event.respond('No host matched that name.') if matches.empty?
+      return event.respond("More than one host matched that name: #{matches.map { |id| host_display_name(id) }.join(', ')}") if matches.size > 1
+
+      ids = matches
+    end
 
     Setting.hosts_ids = Setting.hosts_ids - ids
-    event.respond("Host list updated. Removed: #{ids.map { |id| "<@#{id}>" }.join(', ')}")
+    event.respond("Host list updated. Removed: #{ids.map { |id| host_display_name(id) }.join(', ')}")
   end
 end
